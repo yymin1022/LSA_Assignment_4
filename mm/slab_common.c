29b91eca7c77f2b1a00d67c27bd2f712885689b2
yymin1022 <yymin1022@gmail.com>
2023-11-21 01:51:44 +0000

    Update slab_common.c : Fixed kmalloc_caches Size Difference Issue

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1110,7 +1110,7 @@ struct kmem_cache *__init create_kmalloc_cache(const char *name,
 }
 
 struct kmem_cache *
-kmalloc_caches[NR_KMALLOC_TYPES][KMALLOC_SHIFT_HIGH + 1] __ro_after_init =
+kmalloc_caches[NR_KMALLOC_TYPES][KMALLOC_SHIFT_HIGH + 2] __ro_after_init =
 { /* initialization for https://bugs.llvm.org/show_bug.cgi?id=42570 */ };
 EXPORT_SYMBOL(kmalloc_caches);
 
