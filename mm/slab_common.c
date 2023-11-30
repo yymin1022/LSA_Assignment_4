d999b10a75c4af62b3476396b02be7aa0617cb97
yymin1022 <yymin1022@gmail.com>
2023-11-30 18:34:06 +0000

    Update slab_common.c : Fixed 384 Size kmalloc Index

--- mm/slab_common.c
+++ mm/slab_common.c
@@ -1187,13 +1187,14 @@ const struct kmalloc_info_struct kmalloc_info[] __initconst = {
 	{"kmalloc-256",           256},		{"kmalloc-512",           512},
 	{"kmalloc-1k",           1024},		{"kmalloc-2k",           2048},
 	{"kmalloc-4k",           4096},		{"kmalloc-8k",           8192},
+	{"kmalloc-384",	          384},
 	{"kmalloc-16k",         16384},		{"kmalloc-32k",         32768},
 	{"kmalloc-64k",         65536},		{"kmalloc-128k",       131072},
 	{"kmalloc-256k",       262144},		{"kmalloc-512k",       524288},
 	{"kmalloc-1M",        1048576},		{"kmalloc-2M",        2097152},
 	{"kmalloc-4M",        4194304},		{"kmalloc-8M",        8388608},
 	{"kmalloc-16M",      16777216},		{"kmalloc-32M",      33554432},
-	{"kmalloc-64M",      67108864},		{"kmalloc-384",           384}
+	{"kmalloc-64M",      67108864}
 };
 
 /*
